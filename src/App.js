5e7b4e39d411d1f6b7a9f0447966a83bd1e0b591
Eduardo Perez <edujpp1@gmail.com>
2019-07-23 11:40:44 -0300

    Se crearon nuevos hooks con useState para calcular la cantidad de paginas

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
 
   const[busqueda, guardarBusqueda] = useState('');
   const[imagenes, guardarImagenes] = useState([]);
+  const[paginaActual, guardarPaginaActual] = useState(1);
+  const[totalPaginas, guardarTotalPaginas] = useState(1);
 
   useEffect(() => {
 
@@ -21,6 +23,10 @@ function App() {
       const resultado = await respuesta.json();
 
       guardarImagenes(resultado.hits);
+
+      // Calcular el total de paginas
+      const calcularTotalPaginas = Math.ceil(resultado.totalHits/imagenesPorPagina);
+      guardarTotalPaginas(calcularTotalPaginas);
     }
     consultarAPI();
 
